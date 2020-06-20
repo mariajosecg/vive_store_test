4abd381e1147e4dcb6edba3d8d76aa8bead99825
mariajosecg <castrogonzalezmariajose6@gmail.com>
2020-06-20 16:24:21 -0400

    Testing sidekiq connection

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDISTOGO_URL"] }
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: ENV["REDISTOGO_URL"] }
+# end
+
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: ENV["REDISTOGO_URL"] }
+# end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDISTOGO_URL"] }
+	config.redis = { :size => 10 }
+end
+
+Sidekiq.configure_server do |config|
+	config.redis = { :size => 4 }
 end
