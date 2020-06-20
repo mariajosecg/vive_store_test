ef0f87d792e4b732c1a5a59cfec7b482d534b40b
mariajosecg <castrogonzalezmariajose6@gmail.com>
2020-06-20 17:01:40 -0400

    Testing sidekiq connection

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,9 +2,9 @@
 require 'sidekiq'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDISTOGO_URL"], size: 1 }
+  config.redis = { url: ENV["REDISTOGO_URL"] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDISTOGO_URL"], size: 2 }
+  config.redis = { url: ENV["REDISTOGO_URL"] }
 end
