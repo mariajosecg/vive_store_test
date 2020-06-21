8989d66fae6d5270cdb397d7d162bf45a421deff
mariajosecg <castrogonzalezmariajose6@gmail.com>
2020-06-20 23:37:47 -0400

    Validate than price must be greather tan 0D

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -3,6 +3,14 @@
 # class Variant
 class Variant < ApplicationRecord
   validates_presence_of :name, :price
-  validates_numericality_of :price, greather_than: 0
+  validate :price, :greather_than_zero
   belongs_to :product
+
+  private
+
+  def greather_than_zero
+		if self.price.to_i <= 0
+			errors.add(:price, "Must be greather than 0")
+		end
+  end
 end
